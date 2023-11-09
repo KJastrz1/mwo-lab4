e60b9e66189697d5cdce371c71ba55547ffb131a
Krzysztof Jastrzębski <krzysztof@jastrzebski>
2023-11-09 21:29:06 +0100

    ignore

---
+++ TestProject1/CalculatorTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using mwolab4;
+using NUnit.Framework;
+
+[TestFixture]
+public class CalculatorTests
+{
+    private Calculator cal=new Calculator();
+    [Test]
+    public void AddTwoIntegers_PositiveNumbers_ReturnsSum()
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 20;
+
+        // Act
+        int result = cal.AddTwoIntegers(num1, num2);
+
+        // Assert
+        Assert.AreEqual(30, result);
+    }
+
+    [Test]
+    public void AddTwoIntegers_NegativeNumber_ThrowsArgumentException()
+    {
+        // Arrange
+        int num1 = -10;
+        int num2 = 20;
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => cal.AddTwoIntegers(num1, num2));
+    }
+
+    [Test]
+    public void AddTwoIntegers_BothNegativeNumbers_ThrowsArgumentException()
+    {
+        // Arrange
+        int num1 = -10;
+        int num2 = -20;
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => cal.AddTwoIntegers(num1, num2));
+    }
+}
---
+++ mwolab4/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mwolab4
+{
+    public class Calculator
+    {
+        public int AddTwoIntegers(int a, int b)
+        {
+            if (a < 0 || b < 0)
+            {
+                throw new ArgumentException("Both integers must be positive.");
+            }
+
+            return a + b;
+        }
+    }
+}
